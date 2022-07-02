c90db286565db03770473eeedc68def708bc7388
hellodajana <dajanasmolyakov@gmail.com>
2022-07-02 14:32:34 +0200

    useState

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
-import React from "react";
 import { render } from "react-dom";
-import Pet from "./pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Coco" animal="dog" breed="poodle" />
-      <Pet name="Lily" animal="dog" breed="shar pei" />
-      <Pet name="Lucy" animal="cat" breed="mixed" />
+      <SearchParams />;
     </div>
   );
 };
